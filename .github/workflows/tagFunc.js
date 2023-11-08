b8c17dd1dc29ae44c5dbec3be2612e41b7c0c0b5
Pickysaurus <pickysaurus@googlemail.com>
2023-11-08 17:46:48 +0000

    Action tweaks again (why is it so hard to inspect the objects passed?!)

--- .github/workflows/tagFunc.js
+++ .github/workflows/tagFunc.js
@@ -2,6 +2,12 @@ module.exports = ({ github, context }) => {
     const issueSummary = context.issue;
     const issueFull = context.payload.issue;
     console.log('GitHub issue opened', { issueSummary, issueFull });
+
+    // github.rest.issue.update({
+    //     issue_number: context.issue.number,
+    //     owner: context.repo.owner,
+    //     repo: context.repo.repo,
+    // })
     // github.rest.issues.addLabels({
     //     issue_number: context.issue.number,
     //     owner: context.repo.owner,
