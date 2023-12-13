8d83a83e6227c0e5cd8a58be94f8b2959ea8cc95
Pickysaurus <pickysaurus@googlemail.com>
2023-12-13 19:07:42 +0000

    Made the JS async

--- .github/workflows/generateAssets.js
+++ .github/workflows/generateAssets.js
@@ -1,7 +1,7 @@
 module.exports = async ({ github, context }) => {
-    const fs = require('fs');
+    const fs = require('fs/promises');
     const path = require('path');
-    const directory = fs.readdir(path.join('./',__dirname));
+    const directory = await fs.readdir(path.join('./',__dirname));
     console.log('Directory', { directory });
     
 }
