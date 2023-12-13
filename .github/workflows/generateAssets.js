07745088f6352a61cdda787a85eabdd28416e730
Pickysaurus <pickysaurus@googlemail.com>
2023-12-13 19:13:45 +0000

    Resolve path

--- .github/workflows/generateAssets.js
+++ .github/workflows/generateAssets.js
@@ -1,7 +1,7 @@
 module.exports = async ({ github, context }) => {
     const fs = require('fs/promises');
     const path = require('path');
-    const folderPath = path.join('../../',__dirname)
+    const folderPath = path.resolve(__dirname, '..', '..')
     const directory = await fs.readdir(folderPath);
     console.log('Directory', { folderPath, directory });
     
