f37572a4557919159a373bc2716713e60552f601
Pickysaurus <pickysaurus@googlemail.com>
2023-12-14 17:33:23 +0000

    Logging again

--- .github/workflows/versionTag.js
+++ .github/workflows/versionTag.js
@@ -1,6 +1,6 @@
 module.exports = async (type, input) => {
     try {
-        console.log('Checking version/tag', { type, input })
+        console.log('Checking '+type, input)
         if (type === 'tag') return (input ?? 'nightly').toLowerCase()
         else if (type === 'version') return (input ?? 'Nightly')
         else throw new Error('Type must be either "tag" or "version"')
