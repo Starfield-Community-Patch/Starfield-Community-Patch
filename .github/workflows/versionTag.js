964372470975eb04887957ec44ca79a1df955668
Pickysaurus <pickysaurus@googlemail.com>
2023-12-14 17:30:44 +0000

    Logging

--- .github/workflows/versionTag.js
+++ .github/workflows/versionTag.js
@@ -1,6 +1,12 @@
-module.exports = (type, input) => {
-    console.log('Checking version/tag', { type, input })
-    if (type === 'tag') return (input ?? 'nightly').toLowerCase()
-    else if (type === 'version') return (input ?? 'Nightly')
-    else throw new Error('Type must be either "tag" or "version"')
+module.exports = async (type, input) => {
+    try {
+        console.log('Checking version/tag', { type, input })
+        if (type === 'tag') return (input ?? 'nightly').toLowerCase()
+        else if (type === 'version') return (input ?? 'Nightly')
+        else throw new Error('Type must be either "tag" or "version"')
+    }
+    catch(err) {
+        console.warn(err);
+        return 'nightly'
+    }
 }
