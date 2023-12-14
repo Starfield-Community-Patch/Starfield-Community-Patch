d54345723f3de11b0215694abfc0d660bd3178ad
Pickysaurus <pickysaurus@googlemail.com>
2023-12-14 17:25:32 +0000

    Fixed JS error

--- .github/workflows/versionTag.js
+++ .github/workflows/versionTag.js
@@ -1,6 +1,6 @@
 module.exports = (type, input) => {
     console.log('Checking version/tag', { type, input })
     if (type === 'tag') return (input ?? 'nightly').toLowerCase()
-    else if (type 'version') return (input ?? 'Nightly')
+    else if (type === 'version') return (input ?? 'Nightly')
     else throw new Error('Type must be either "tag" or "version"')
 }
